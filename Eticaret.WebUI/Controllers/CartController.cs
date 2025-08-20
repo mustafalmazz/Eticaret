02802b360f04d50ff1c1338bfe0117e6c5784991
mustafalmazz <mustafagfb19077091@gmail.com>
2025-08-20 16:00:17 +0300

    Sepet İşlemleri

--- Eticaret.WebUI/Controllers/CartController.cs
+++ Eticaret.WebUI/Controllers/CartController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Eticaret.Core.Entities;
+using Eticaret.Service.Abstract;
+using Eticaret.Service.Concrete;
+using Eticaret.WebUI.ExtensionMethods;
+using Eticaret.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Eticaret.WebUI.Controllers
 {
     public class CartController : Controller
     {
+        private readonly IService<Product> _service;
+
+        public CartController(IService<Product> service)
+        {
+            _service = service;
+        }
         public IActionResult Index()
         {
-            return View();
+            var cart = GetCart();
+            var model = new CartViewModel()
+            {
+                CartLines = cart.CartLines,
+                TotalPrice = cart.TotalPrice()
+            };
+            return View(model);
+        }
+        private CartService GetCart()
+        {
+            return HttpContext.Session.GetJson<CartService>("Cart") ?? new CartService();
+        }
+        public IActionResult Add(int id,int quantity = 1)
+        {
+            var product = _service.Find(id);
+            if (product != null)
+            {
+                var cart = GetCart();
+                cart.AddProduct(product , quantity);
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Update(int id,int quantity = 1)
+        {
+            var product = _service.Find(id);
+            if (product != null)
+            {
+                var cart = GetCart();
+                cart.UpdateProduct(product , quantity);
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Remove(int ProductId)
+        {
+            var product = _service.Find(ProductId);
+            if (product != null)
+            {
+                var cart = GetCart();
+                cart.RemoveProduct(product);
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
